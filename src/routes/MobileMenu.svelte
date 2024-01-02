c4b6a507550afab0cf4e5d37f58383417f8e43d4
BoilingSoup <boilingsoup@protonmail.com>
2024-01-01 17:37:01 -0800

    add menu transition

--- src/routes/MobileMenu.svelte
+++ src/routes/MobileMenu.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  //
+  import { slide } from "svelte/transition";
 </script>
 
 <main>
-  <div class="z-90 fixed h-full w-full bg-red-500"></div>
+  <div class="z-90 fixed h-full w-full bg-zinc-400/90 dark:bg-black/50" transition:slide={{ axis: "x" }}></div>
 </main>
