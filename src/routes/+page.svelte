4b960a99d04a9cb5047393bdeb57195bae3dfa66
BoilingSoup <boilingsoup@protonmail.com>
2024-01-03 07:08:38 -0800

    style home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,30 @@
 <script>
+  import { darkModeStore } from "$lib";
   import BSoupFrog from "./BSoupFrog.svelte";
   import PrettyLink from "./PrettyLink.svelte";
+
+  $: frogColor = $darkModeStore ? "#f1f5f9" : "#27272a";
 </script>
 
 <svelte:head>
   <title>BoilingSoup | Home</title>
 </svelte:head>
 
-<main class="flex h-[calc(100%_-_theme(space.14))] flex-col items-center justify-center gap-14">
-  <h1 class="text-5xl text-black transition ease-in dark:text-white">Hi, I'm BoilingSoup</h1>
-
-  <div class="flex items-center gap-14">
-    <BSoupFrog width={150} height={150} />
-
-    <div class="flex flex-col gap-5">
-      <!-- <p class="text-xl text-black transition ease-in dark:text-white">Check out my:</p> -->
-      <PrettyLink href="https://github.com/boilingsoup" text="GitHub" targetBlank={true} />
-      <PrettyLink href="/projects" text="Projects" />
-      <PrettyLink href="/blog" text="Blog" />
+<main class="h-[calc(100%_-_theme(space.14))]">
+  <div class={"flex h-full items-center justify-center"}>
+    <div class="text-center">
+      <p class="mt-2 text-left text-3xl text-zinc-800 transition ease-in-out dark:text-slate-200 lg:text-7xl">
+        Hi, I'm BoilingSoup.<br />Welcome to my portfolio!
+      </p>
+      <div class="mt-28 flex items-end justify-between [&>svg]:h-[100px] [&>svg]:w-[100px] lg:[&>svg]:h-[300px] lg:[&>svg]:w-[300px]">
+        <BSoupFrog color={frogColor} />
+        <div class="flex w-1/2 flex-col gap-5">
+          <p class="text-center text-2xl font-bold text-black transition ease-in dark:text-white">Check out my 👇</p>
+          <PrettyLink href="/projects" text="Projects" />
+          <PrettyLink href="/blog" text="Blog" />
+          <PrettyLink href="https://github.com/boilingsoup" text="GitHub" targetBlank={true} />
+        </div>
+      </div>
     </div>
   </div>
 </main>
--- src/routes/PrettyLink.svelte
+++ src/routes/PrettyLink.svelte
@@ -8,13 +8,13 @@
   <a
     {href}
     target="_blank"
-    class="rounded-xl border-4 border-gray-600 px-8 py-2 text-center text-2xl font-bold text-black transition ease-in dark:border-slate-500 dark:text-white"
+    class="rounded-xl border-4 border-gray-600 bg-slate-50 px-12 py-4 text-center text-2xl font-bold text-black transition ease-in-out hover:bg-slate-400 dark:border-slate-500 dark:bg-slate-600/40 dark:text-white hover:dark:bg-slate-500"
     >{text}</a
   >
 {:else}
   <a
     {href}
-    class="rounded-xl border-4 border-gray-600 px-8 py-2 text-center text-2xl font-bold text-black transition ease-in dark:border-slate-500 dark:text-white"
+    class="rounded-xl border-4 border-gray-600 bg-slate-50 px-12 py-4 text-center text-2xl font-bold text-black transition ease-in-out hover:bg-slate-400 dark:border-slate-500 dark:bg-slate-600/40 dark:text-white hover:dark:bg-slate-500"
     >{text}</a
   >
 {/if}
