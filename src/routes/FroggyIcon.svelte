9b0ea82e7cc9251926bc284226625ccaa6b4e368
BoilingSoup <boilingsoup@protonmail.com>
2024-01-06 16:30:51 -0800

    style header contents

--- src/routes/FroggyIcon.svelte
+++ src/routes/FroggyIcon.svelte
@@ -21,13 +21,13 @@
   on:blur={removeHoverEffect}
 >
   <div
-    class="absolute left-1 top-1 lg:left-1 [&>svg]:mx-4 [&>svg]:mt-2 [&>svg]:hidden [&>svg]:h-[30px] [&>svg]:w-[30px] [&>svg]:animate-fade-in-up [&>svg]:transition [&>svg]:ease-in-out [&>svg]:2xs:inline [&>svg]:lg:ml-8"
+    class="absolute left-1 top-1 lg:left-1 [&>svg]:mt-2 [&>svg]:hidden [&>svg]:h-[30px] [&>svg]:w-[30px] [&>svg]:animate-fade-in-up [&>svg]:transition [&>svg]:ease-in-out [&>svg]:2xs:mx-2 [&>svg]:2xs:inline [&>svg]:lg:ml-8"
   >
     <BSoupFrog />
   </div>
   <div
     bind:this={hoverEffect}
     id="hover-effect"
-    class="absolute bottom-0 left-4 z-10 h-6 w-[40px] before:absolute before:bottom-0 before:h-[0px] before:w-[100%] before:rounded before:bg-gray-900 before:opacity-75 before:transition-all before:duration-200 before:content-[''] before:dark:bg-slate-50 lg:left-8"
+    class="absolute bottom-0 left-2 z-10 h-6 w-[40px] before:absolute before:bottom-0 before:h-[0px] before:w-[100%] before:rounded before:bg-gray-900 before:opacity-75 before:transition-all before:duration-200 before:content-[''] before:dark:bg-slate-50 lg:left-8"
   />
 </a>
