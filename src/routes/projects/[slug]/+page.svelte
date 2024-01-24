b49be6b69db19cfa1e41a14f2a2e6b420b2ede80
BoilingSoup <boilingsoup@protonmail.com>
2024-01-24 14:40:24 -0800

    update project page styles

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -2,6 +2,7 @@
   import NewTabIcon from "../../NewTabIcon.svelte";
   import { DOMAIN } from "../../constants";
   import Badge from "../Badge.svelte";
+  import ProjectLabel from "./ProjectLabel.svelte";
 
   export let data;
 </script>
@@ -11,45 +12,50 @@
 </svelte:head>
 
 <main class="mx-auto mt-8 rounded bg-slate-300 transition ease-in-out lg:container dark:bg-zinc-900">
-  <!-- <h1 class="text-4xl text-black dark:text-white">hello!</h1> -->
-  <!-- <h1 class="text-4xl text-black dark:text-white">{data.id}</h1> -->
   <div class="px-8 py-5">
     <h1
-      class="rounded-lg bg-slate-300 py-6 text-center text-2xl font-bold text-black transition ease-in-out dark:bg-zinc-900 dark:text-white lg:text-4xl"
+      class="rounded-lg bg-slate-300 py-6 text-center text-2xl font-bold text-black transition ease-in-out dark:bg-zinc-900 dark:text-white xs:text-3xl lg:text-4xl"
     >
       {data.title}
-      <a class="text-blue-700 underline transition ease-in-out dark:text-blue-400" href={data.link.url} target="_blank"
-        >{data.link.text ?? data.link.url}<span class="h-[30px] w-[30px]"><NewTabIcon --size="30px" /></span></a
+      <a
+        class="whitespace-nowrap text-xl text-blue-700 underline transition ease-in-out dark:text-blue-400 xs:text-2xl lg:text-3xl"
+        href={data.link.url}
+        target="_blank">{data.link.text ?? data.link.url}<span class="h-[30px] w-[30px]"><NewTabIcon --size="30px" /></span></a
       >
-      <!-- <a -->
-      <!--   href={data.link.url} -->
-      <!--   class="visit-site-link absolute right-0 rounded-md border border-black px-6 py-4 text-2xl font-bold text-black transition ease-in-out dark:border-white dark:text-white" -->
-      <!--   target="_blank">Visit Site</a -->
-      <!-- > -->
     </h1>
-    <div class="flex flex-col justify-center gap-4 py-6 lg:flex-row lg:gap-10">
-      <h2 class="text-black transition ease-in-out dark:text-white lg:text-xl">
-        Languages: {#each data.techStack.languages as language}
-          <Badge color={language.color}>{language.name}</Badge>
-        {/each}
-      </h2>
-      <h2 class="text-black transition ease-in-out dark:text-white lg:text-xl">
-        Frameworks: {#each data.techStack.frameworks as framework}
-          <Badge color={framework.color}>{framework.name}</Badge>
-        {/each}
-      </h2>
+    <div class="flex flex-col justify-center gap-4 py-6 xl:flex-row xl:gap-10">
       <div class="flex gap-2">
-        <h2 class="text-black transition ease-in-out dark:text-white lg:text-xl">
-          Source Code:
-          {#if !data.source.public}
-            <Badge color="bg-gray-700">Private</Badge>
-          {/if}
-        </h2>
+        <div class="w-[14ch] text-right xl:w-auto">
+          <ProjectLabel>Languages:</ProjectLabel>
+        </div>
+        <div class="flex h-min gap-1">
+          {#each data.techStack.languages as language}
+            <Badge color={language.color}>{language.name}</Badge>
+          {/each}
+        </div>
+      </div>
+      <div class="flex gap-2">
+        <div class="w-[14ch] text-right xl:w-auto">
+          <ProjectLabel>Frameworks:</ProjectLabel>
+        </div>
+        <div class="flex h-min gap-1">
+          {#each data.techStack.frameworks as framework}
+            <Badge color={framework.color}>{framework.name}</Badge>
+          {/each}
+        </div>
+      </div>
+      <div class="flex gap-2">
+        <div class="w-[14ch] text-right xl:w-auto">
+          <ProjectLabel>Source Code:</ProjectLabel>
+        </div>
+        {#if !data.source.public}
+          <Badge color="bg-gray-700">Private</Badge>
+        {/if}
         {#if data.source.public}
           <ul class="list-none">
             {#each data.source.urls as link}
               <li>
-                <a class="text-blue-700 underline transition ease-in-out dark:text-blue-400" target="_blank" href={link}
+                <a class="break-all text-xs text-blue-700 underline transition ease-in-out dark:text-blue-400" target="_blank" href={link}
                   >{link}<span><NewTabIcon --size="20px" /></span></a
                 >
               </li>
@@ -57,11 +63,6 @@
           </ul>
         {/if}
       </div>
-      <!-- <h2 class="text-xl text-black transition ease-in-out dark:text-white"> -->
-      <!--   Live URL: <a class="text-base text-blue-700 underline transition ease-in-out dark:text-blue-400" href={data.url} target="_blank" -->
-      <!--     >{data.url}</a -->
-      <!--   > -->
-      <!-- </h2> -->
     </div>
   </div>
   <div class="flex w-full flex-wrap px-8 py-8">
@@ -86,24 +87,4 @@
   video::-webkit-media-controls-mute-button {
     display: none;
   }
-
-  /* .visit-site-link { */
-  /*   color: rgba(255, 255, 255, 0.9); */
-  /*   background: linear-gradient(-45deg, #ffa63d, #ff3d77, #338aff, #3cf0c5); */
-  /*   background-size: 600%; */
-  /*   animation: animateLinkButton 16s linear infinite; */
-  /*   filter: blur(30px) */
-  /* } */
-
-  /* @keyframes animateLinkButton { */
-  /*   0% { */
-  /*     background-position: 0% 50%; */
-  /*   } */
-  /*   50% { */
-  /*     background-position: 100% 50%; */
-  /*   } */
-  /*   100% { */
-  /*     background-position: 0% 50%; */
-  /*   } */
-  /* } */
 </style>
---
+++ src/routes/projects/[slug]/ProjectLabel.svelte
@@ -0,0 +1,1 @@
+<h2 class="text-black transition ease-in-out dark:text-white xs:text-lg lg:text-xl"><slot /></h2>
