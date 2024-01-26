902d269a300d72f0be63c79067059d6c5607c93d
BoilingSoup <boilingsoup@protonmail.com>
2024-01-26 14:33:06 -0800

    update project layout styles

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -13,63 +13,65 @@
   <title>{DOMAIN} | {data.title}</title>
 </svelte:head>
 
-<main class="mx-auto mt-8 rounded bg-slate-300 py-12 transition ease-in-out lg:container dark:bg-zinc-900">
-  <div class="px-8">
-    <h1
-      class="rounded-lg bg-slate-300 text-center text-2xl font-bold text-black transition ease-in-out dark:bg-zinc-900 dark:text-white xs:text-3xl lg:text-4xl"
-    >
-      {data.title}
-      <a class="link-light-dark whitespace-nowrap text-xl underline xs:text-2xl lg:text-3xl" href={data.link.url} target="_blank"
-        >{data.link.text ?? data.link.url}<span class="h-[30px] w-[30px]"><NewTabIcon --size="30px" /></span></a
+<div class="flex h-[calc(100%-theme(space.14))] w-full flex-wrap place-items-center">
+  <main class="mx-auto rounded bg-slate-300 py-12 transition ease-in-out lg:container dark:bg-zinc-900">
+    <div class="px-8">
+      <h1
+        class="rounded-lg bg-slate-300 text-center text-2xl font-bold text-black transition ease-in-out dark:bg-zinc-900 dark:text-white xs:text-3xl lg:text-4xl"
       >
-    </h1>
-    <div class="flex flex-col justify-center gap-4 py-6 md:flex-row xl:gap-10">
-      <div class="flex flex-col gap-4 lg:hidden">
-        <Language {data} />
-        <Framework {data} />
-      </div>
-      <div class="hidden lg:block">
-        <Language {data} />
-      </div>
-      <div class="hidden lg:block">
-        <Framework {data} />
-      </div>
-      <div class="flex gap-2">
-        <div class="w-[14ch] text-right">
-          <ProjectLabel>Source Code:</ProjectLabel>
+        {data.title}
+        <a class="link-light-dark whitespace-nowrap text-xl underline xs:text-2xl lg:text-3xl" href={data.link.url} target="_blank"
+          >{data.link.text ?? data.link.url}<span class="h-[30px] w-[30px]"><NewTabIcon --size="30px" /></span></a
+        >
+      </h1>
+      <div class="flex flex-col justify-center gap-4 overflow-hidden py-8 md:py-20 xl:flex-row xl:gap-10">
+        <div class="flex flex-col gap-4 lg:hidden">
+          <Language {data} />
+          <Framework {data} />
         </div>
-        {#if !data.source.public}
-          <div class="h-min">
-            <Badge color="bg-gray-700">Private</Badge>
+        <div class="hidden lg:block">
+          <Language {data} />
+        </div>
+        <div class="hidden lg:block">
+          <Framework {data} />
+        </div>
+        <div class="flex gap-2">
+          <div class="w-[14ch] text-right">
+            <ProjectLabel>Source Code:</ProjectLabel>
           </div>
-        {/if}
-        {#if data.source.public}
-          <ul class="list-none overflow-hidden text-ellipsis whitespace-nowrap">
-            {#each data.source.urls as link}
-              <li>
-                <a class="link-light-dark break-all text-xs underline transition ease-in-out xl:text-sm" target="_blank" href={link}
-                  >{link}<span><NewTabIcon --size="20px" /></span></a
-                >
-              </li>
-            {/each}
-          </ul>
-        {/if}
+          {#if !data.source.public}
+            <div class="h-min">
+              <Badge color="bg-gray-700">Private</Badge>
+            </div>
+          {/if}
+          {#if data.source.public}
+            <ul class="list-none overflow-hidden text-ellipsis whitespace-nowrap">
+              {#each data.source.urls as link}
+                <li>
+                  <a class="link-light-dark break-all text-xs underline transition ease-in-out md:text-sm" target="_blank" href={link}
+                    >{link}<span><NewTabIcon --size="20px" /></span></a
+                  >
+                </li>
+              {/each}
+            </ul>
+          {/if}
+        </div>
       </div>
     </div>
-  </div>
-  <div class="flex w-full flex-wrap-reverse gap-8 px-8 md:flex-wrap">
-    <div class="xl:w-[calc(50%-theme(space.4))]">
-      <h2 class="text-3xl text-black transition ease-in-out dark:text-white">About</h2>
-      <p class="whitespace-pre-wrap bg-slate-300 text-black transition ease-in-out dark:bg-zinc-900 dark:text-white">
-        {@html data.description}
-      </p>
+    <div class="flex w-full flex-wrap-reverse gap-8 px-8 md:flex-wrap">
+      <div class="xl:w-[calc(50%-theme(space.4))]">
+        <h2 class="text-xl font-bold text-black transition ease-in-out dark:text-white">About</h2>
+        <p class="whitespace-pre-wrap bg-slate-300 text-black transition ease-in-out dark:bg-zinc-900 dark:text-white">
+          {@html data.description}
+        </p>
+      </div>
+      <div class="xl:w-[calc(50%-theme(space.4))]">
+        <!-- svelte-ignore a11y-media-has-caption -->
+        <video class="w-full max-w-4xl" src={data.video} title="demo" loop controls />
+      </div>
     </div>
-    <div class="xl:w-[calc(50%-theme(space.4))]">
-      <!-- svelte-ignore a11y-media-has-caption -->
-      <video class="w-full max-w-4xl" src={data.video} title="demo" loop controls />
-    </div>
-  </div>
-</main>
+  </main>
+</div>
 
 <style>
   video::-webkit-media-controls-volume-slider {
--- src/routes/projects/[slug]/ProjectLabel.svelte
+++ src/routes/projects/[slug]/ProjectLabel.svelte
@@ -1,1 +1,1 @@
-<h2 class="text-black transition ease-in-out dark:text-white xs:text-lg lg:text-xl"><slot /></h2>
+<h2 class="font-bold text-black transition ease-in-out dark:text-white lg:text-xl"><slot /></h2>
