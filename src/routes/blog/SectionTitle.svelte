d70eb92cab69c852d43663ba19d1e3df9af96410
BoilingSoup <boilingsoup@protonmail.com>
2024-01-29 10:59:30 -0800

    update styles

--- src/routes/blog/SectionTitle.svelte
+++ src/routes/blog/SectionTitle.svelte
@@ -4,5 +4,5 @@
 </script>
 
 <a id={hash} href="#{hash}" class="transition ease-in-out">
-  <h2 class="text-3xl font-bold text-gray-800 transition ease-in-out dark:text-blue-400">{title}</h2>
+  <h2 class="text-3xl font-bold text-sky-900 transition ease-in-out dark:text-blue-400">{title}</h2>
 </a>
